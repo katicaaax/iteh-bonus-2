a548b61d86272bd14ec7ef813a6087d1e5887aa1
katicaaax <58169122+katicaaax@users.noreply.github.com>
2021-10-31 20:49:52 +0100

    Add files via upload

---
+++ iteh domaci 2/handler/update.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+require "../dbBroker.php";
+require "../model/prijava.php";
+
+if (
+    isset($_POST['katedra']) && isset($_POST['predmet'])
+    && isset($_POST['sala']) && isset($_POST['datum']) && isset($_POST['id'])
+) {
+    $obj = new Prijava(null, $_POST['predmet'], $_POST['katedra'], $_POST['sala'], $_POST['datum']);
+    $status = $obj->update($_POST['id'], $conn);
+    if ($status) {
+        echo "Success";
+    } else {
+        echo "Failed";
+    }
+}
+
+?> 
